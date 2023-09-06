6a48f5bf150259974e14ef4552075800c454f708
Ivan-Niki <iFankov@gmail.com>
2023-09-07 02:51:19 +0300

    add ShortLongShort task

--- codewars-tasks.js
+++ codewars-tasks.js
@@ -2290,3 +2290,28 @@ function strCount(str, letter){
   return count;
 }
 
+// 123. Short Long Short
+//Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and //the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+//Hint for R users:
+//The length of string is not always the same as the number of characters
+//For example: (Input1, Input2) --> output
+
+//("1", "22") --> "1221"
+//("22", "1") --> "1221"
+
+function solution(a, b){
+  let result;
+  if (a.length < b.length) {
+    result = a + b + a;
+  } else if (b.length < a.length) {
+    result = b + a + b;
+  }
+  return result;
+}
+
+// 2nd version:
+function solution(a, b) {
+  return a.length < b.length ? a + b + a : b + a + b
+}
+
+
