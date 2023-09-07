32b78b450cd8c05c5ef03acddd396ec280391336
Ivan-Niki <iFankov@gmail.com>
2023-09-08 01:55:53 +0300

    add String-ends-with task

--- codewars-tasks.js
+++ codewars-tasks.js
@@ -2315,3 +2315,14 @@ function solution(a, b) {
 }
 
 
+// 124. String ends with?
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument //(also a string).
+//Examples:
+//solution('abc', 'bc') // returns true
+//solution('abc', 'd') // returns false
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+
