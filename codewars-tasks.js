718268fbc1c5a8c289594d07b9aa2d63cad59174
Ivan-Niki <iFankov@gmail.com>
2023-09-08 12:03:46 +0300

    add Fake-Binary task

--- codewars-tasks.js
+++ codewars-tasks.js
@@ -2348,3 +2348,17 @@ function stringy(size) {
   return string
 }
 
+
+// 126. Fake Binary
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return //the resulting string.
+//Note: input will never be an empty string
+
+function fakeBin(x){ 
+  let resultStr = x.replace(/[1-4]/g, "0");
+  return resultStr.replace(/[5-9]/g, "1")
+}
+
+// 2nd version:
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
